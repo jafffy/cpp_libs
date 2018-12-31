2fbdd0a29f34338d14e41411df7f6b26adb8e602
Jaewon James Choi <jaewon.james.choi@gmail.com>
2018-12-31 23:25:34 +0900

    Add future error

---
+++ future/future_error.cc
@@ -0,0 +1,15 @@
+#include <future>
+#include <iostream>
+
+int main()
+{
+  std::future<int> empty;
+  try {
+    int n = empty.get();
+  } catch (const std::future_error& e) {
+    std::cout << "Caught a future_error with code \"" << e.code()
+      << "\"\nMessage: \"" << e.what() << "\"\n";
+  }
+
+  return 0;
+}
