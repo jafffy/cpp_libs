577fb89fc8b6d9e8222c702681e8c745c6aaf48d
Jaewon Choi <jaewon.james.choi@gmail.com>
2019-01-21 21:10:26 +0900

    Add decltype

---
+++ decltype.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+
+struct A { double x; };
+const A* a;
+
+decltype(a->x) y;
+decltype((a->x)) z = y;
+
+template <typename T, typename U>
+auto add(T t, U u) -> decltype(t + u)
+{
+  return t + u;
+}
+
+int main()
+{
+  int i = 33;
+  decltype(i) j = i * 2;
+
+  std::cout << "i = " << i << ", "
+            << "j = " << j << '\n';
+
+  auto f = [](int a, int b) -> int
+  {
+    return a * b;
+  };
+
+  decltype(f) g = f;
+  i = f(2, 2);
+  j = g(3, 3);
+
+  std::cout << "i = " << i << ", "
+            << "j = " << j << '\n';
+
+  return 0;
+}
