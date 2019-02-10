3a331882a77a11ace015acee9a2c365821f35b92
Jaewon Choi <jaewon.james.choi@gmail.com>
2019-02-10 22:16:55 +0900

    Add root_name of filesystem

---
+++ filesystem/root_name.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <filesystem>
+
+namespace fs = std::filesystem;
+
+int main()
+{
+  std::cout << "Current root name is: " << fs::current_path().root_name() << '\n';
+
+  return 0;
+}
